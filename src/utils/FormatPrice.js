1abd3ce98d612b584edcb3dd85a3424ea3f0b74c
Osaroekaa <osaroekaa@example.com>
2025-11-16 15:10:52 +0100

    Deploy to GitHub Pages

--- src/utils/FormatPrice.js
+++
@@ -1,7 +0,0 @@
-export function formatPrice(price) {
-  return new Intl.NumberFormat('en-NG', {
-    style: 'currency',
-    currency: 'NGN'
-  }).format(price);
-}
-// This function formats a given price into Nigerian Naira currency format.
